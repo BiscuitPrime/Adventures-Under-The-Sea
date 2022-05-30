9ee3c378d08488df8105fd0fd1ba1bff33d77ddf
BiscuitPrime <henri.nomico@gmail.com>
2022-05-30 13:37:21 +0200

    Post-merge problem founds and delimited

--- src/myMain.cpp
+++ src/myMain.cpp
@@ -12,11 +12,19 @@ int myMain()
     int height = 1080;
     sf::RenderWindow window(sf::VideoMode(width, height), "SFML window");
 
+    //---------------------------------
+    //Creating the player :
+    std::string playerTexturePath = "../../../../resources/Sprites/Player.png";
+    auto player = Player(playerTexturePath);
+    player.setPosition(sf::Vector2f(0, 0));
+    //Creating the input handler associated to the player :
+    auto inputHandler = InputHandler();
+    //---------------------------------
+
     Tilemap tilemap;
     auto* fileName = (char*)"Tilemap.xml";
     tilemap.buildTilemap(fileName);
     tilemap.draw(window);
-
     window.display();
     while (window.isOpen())
     {
@@ -26,6 +34,14 @@ int myMain()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        //-------------------
+        // CURRENTLY THE FOLLOWING CODE CREATES ISSUES :
+        
+        //inputHandler.handleInput(&player, &window); //each frame we call the inputhandler's handle input that will analyse the inputted data
+        //window.draw(player.getSprite());
+        //window.display();
+        //--------------------
     }
 
     return 0;
